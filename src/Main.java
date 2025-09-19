b7435f9b630c91417779635c67e420f0e190e5d0
KelvinCodesInC <kelvino28@nycstudents.net>
2025-09-19 12:48:45 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) {
         DogWalkCompany dogwalkcompany = new DogWalkCompany();
         int hour = dogwalkcompany.addDogs();
-        DogWalker dogwalker = new DogWalker(4, dogwalkcompany);
+        DogWalker dogwalker = new DogWalker(3, dogwalkcompany);
         
         for (int i = 7; i < 10; i++) {
             dogwalkcompany.addDogs();
         }
 
         System.out.println("$" + dogwalker.dogWalkShift(7,10) + "dollars earned");
+
+        dogwalker = new DogWalker(4, dogwalkcompany);
     }
 }
